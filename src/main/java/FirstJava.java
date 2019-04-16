e3adc9a39b1ce4f148d6bf4828a96fe7b1f89150
Rajesh <raikar@ymail.com>
2019-04-16 02:44:45 -0400

    Adding new files

---
+++ src/main/java/FirstJava.java
@@ -0,0 +1,9 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+String str = "Java";
+if (str.equals("JAVA")){
+     System.out.println("Hello Babu");	
+    }
+}
+}
